1cbdb0990204da3be43598ba7f7361a9fcc35f31
Lion_R8 <282725622+LionR8@users.noreply.github.com>
2026-06-11 18:25:31 -0700

    Site update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("btn")
 const color = document.getElementById("color")
+const rst = document.getElementById("rst")
 
 const hexC = "0123456789ABCDEF"
 
@@ -11,4 +12,17 @@ btn.addEventListener("click", function(){
     }
     document.body.style.backgroundColor = hexColor
     color.innerText = hexColor
+    localStorage.setItem('hex', hexColor)
+})
+
+const hex = localStorage.getItem('hex')
+if (hex) {
+    document.body.style.backgroundColor = hex
+    color.innerText = hex
+}
+
+rst.addEventListener("click", function(){
+    localStorage.removeItem('hex')
+    document.body.style.backgroundColor = '#ADD8E6'
+    color.innerText = '#ADD8E6'
 })
